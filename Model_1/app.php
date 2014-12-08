f5e275af3037cb6a958aac9435fbd62bde24d621
Igor Cordon <relatur@gmail.com>
2014-12-09 03:56:57 +0400

    Вывод концентрации белков

--- Model_1/app.php
+++ Model_1/app.php
@@ -13,8 +13,12 @@ $u = array(false, 0,1,1, 0,1,1, 0,1,1, 0,1,1, 0,1,1, 0,1,1);
 
 for($T = 0; $T < 10; $T++){
 	$u = dydt($u);
-	// Показ строки:
-	$echo = array("T= $T", $pp1_mrna, $pp2_mrna, $pp3_mrna, $pp4_mrna, $pp5_mrna, $pp6_mrna);
+	// Концентрации мРНК:
+	// $echo = array("T= $T", $pp1_mrna, $pp2_mrna, $pp3_mrna, $pp4_mrna, $pp5_mrna, $pp6_mrna);
+	// foreach($echo as $i) echo substr($i, 0, 5) . '	';
+	
+	// Концентрации белков:
+	$echo = array("T= $T", $p2, $p4, $p6);
 	foreach($echo as $i) echo substr($i, 0, 5) . '	';
 	echo "\n";
 }
