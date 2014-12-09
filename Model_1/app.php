2d085e2aebb57d5bb10e7a2d69a7cd0e51d9f28b
Igor Cordon <relatur@gmail.com>
2014-12-10 02:53:38 +0400

    u = u + du

--- Model_1/app.php
+++ Model_1/app.php
@@ -9,14 +9,17 @@ include __DIR__ . '/dydt.php';
 // Начальное состояние системы
 $u = array(false, 0,1,1, 0,1,1, 0,1,1, 0,1,1, 0,1,1, 0,1,1);
 
-for($T = 0; $T < 10; $T++){
-	$u = dydt($u);
+for($T = 0; $T < 20; $T++){
+	$du = dydt($u);
+	foreach($u as $k => $val) $u[$k] = $val + $du[$k];
+
 	// Концентрации мРНК:
-	// $echo = array("T= $T", $pp1_mrna, $pp2_mrna, $pp3_mrna, $pp4_mrna, $pp5_mrna, $pp6_mrna);
-	// foreach($echo as $i) echo substr($i, 0, 5) . '	';
+	$echo = array("T= $T", $pp1_mrna, $pp2_mrna, $pp3_mrna, $pp4_mrna, $pp5_mrna, $pp6_mrna);
+	// foreach($echo as $i) echo substr($i, 0, 6) . '	';
 	
 	// Концентрации белков:
 	$echo = array("T= $T", $p2, $p4, $p6);
-	foreach($echo as $i) echo substr($i, 0, 5) . '	';
+	foreach($echo as $i) echo substr($i, 0, 6) . '	';
 	echo "\n";
 }
+
