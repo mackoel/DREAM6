64b85b61717e356e156384c18ec5e09c9cf7a923
Igor Cordon <relatur@gmail.com>
2014-12-12 00:57:19 +0400

    Реализация 1 модели на R

---
+++ Model_1_R/_state.R
@@ -0,0 +1,27 @@
+# Вектор изменяемого состояния сисемы
+state = c();
+state["pp1_mrna"] <- 1
+state["pp2_mrna"] <- 1
+state["pp3_mrna"] <- 1
+state["pp4_mrna"] <- 1
+state["pp5_mrna"] <- 1
+state["pp6_mrna"] <- 1
+state["p1"] <- 1
+state["p2"] <- 1
+state["p3"] <- 1
+state["p4"] <- 1
+state["p5"] <- 1
+state["p6"] <- 1
+
+# Параметры меняются из тела функции
+cod1 <- 0
+cod2 <- 0
+cod3 <- 0
+cod4 <- 0
+cod5 <- 0
+cod6 <- 0
+rs1 <- 1
+rs2 <- 1
+rs3 <- 1
+rs4 <- 1
+rs5 <- 1
---
+++ Model_1_R/test.R
@@ -0,0 +1,48 @@
+#!/usr/bin/env Rscript
+#install.packages('deSolve', repos='http://probability.ca/cran')
+
+library(deSolve)
+source("_parameters.R") # список параметров  
+source("_state.R")      # начальное состояние системы (вектор)
+options(width = 160)    # ширина вывода на экран 
+
+DyDt <- function(t, u, params){
+    as1 <- ((1+((u["p1"]/p$v2_Kd)^p$v2_h))-1)/(1+((u["p1"]/p$v2_Kd)^p$v2_h));
+    as2 <- ((1+((u["p1"]/p$v1_Kd)^p$v1_h))-1)/(1+((u["p1"]/p$v1_Kd)^p$v1_h));
+    as3 <- ((1+((u["p1"]/p$v3_Kd)^p$v3_h))-1)/(1+((u["p1"]/p$v3_Kd)^p$v3_h));
+
+    cod1 <<- p$pro1_strength;
+    cod2 <<- p$pro2_strength * as1 * rs1;
+    cod3 <<- p$pro3_strength * as3 * rs4;
+    cod4 <<- p$pro4_strength * as2 * rs2;
+    cod5 <<- p$pro5_strength * rs3;
+    cod6 <<- p$pro6_strength * rs5;
+
+    rs1 <<- 1/(1+((u["p6"]/p$v5_Kd)^p$v5_h));
+    rs2 <<- 1/(1+((u["p5"]/p$v8_Kd)^p$v8_h));
+    rs3 <<- 1/(1+((u["p4"]/p$v6_Kd)^p$v6_h));
+    rs4 <<- 1/(1+((u["p2"]/p$v4_Kd)^p$v4_h));
+    rs5 <<- 1/(1+((u["p4"]/p$v7_Kd)^p$v7_h));
+
+	ds = c();
+	ds["pp1_mrna"] <- cod1 - p$pp1_mrna_degradation_rate * u["pp1_mrna"];
+	ds["pp2_mrna"] <- cod2 - p$pp2_mrna_degradation_rate * u["pp2_mrna"];
+	ds["pp3_mrna"] <- cod3 - p$pp3_mrna_degradation_rate * u["pp3_mrna"];
+	ds["pp4_mrna"] <- cod4 - p$pp4_mrna_degradation_rate * u["pp4_mrna"];
+	ds["pp5_mrna"] <- cod5 - p$pp5_mrna_degradation_rate * u["pp5_mrna"];
+	ds["pp6_mrna"] <- cod6 - p$pp6_mrna_degradation_rate * u["pp6_mrna"];
+	ds["p1"] <- p$rbs1_strength * u["pp1_mrna"] - p$p1_degradation_rate * u["p1"];
+	ds["p2"] <- p$rbs2_strength * u["pp2_mrna"] - p$p2_degradation_rate * u["p2"];
+	ds["p3"] <- p$rbs3_strength * u["pp3_mrna"] - p$p3_degradation_rate * u["p3"];
+	ds["p4"] <- p$rbs4_strength * u["pp4_mrna"] - p$p4_degradation_rate * u["p4"];
+	ds["p5"] <- p$rbs5_strength * u["pp5_mrna"] - p$p5_degradation_rate * u["p5"];
+	ds["p6"] <- p$rbs6_strength * u["pp6_mrna"] - p$p6_degradation_rate * u["p6"];
+
+	return(list(ds))
+}
+
+p <- parameters # параметры системы
+times <- seq(0, 10, by = 0.05)
+out <- ode(y = state, times = times, func = DyDt, parms = NULL)
+
+print(out)
